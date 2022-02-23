54de9680978d33de65f3219166aeea14a0af504a
Samuel Pereira Brandão <samuelpbrandao58@gmail.com>
2022-02-23 14:23:14 -0300

    xss

--- crud/php_action/create.php
+++ crud/php_action/create.php
@@ -3,15 +3,24 @@
 session_start();
 //conexão
 require_once 'db_connect.php';
+//clear
+function clear($input){
+    global $connect;
+    //sql
+    $var = mysqli_escape_string($connect,$input);
+    //xss (cross site scripting)
+    $var = htmlspecialchars($var);
+    return $var;
+}
 if(isset($_POST['btn-cadastrar'])){
     if (!$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)) {
 		$_SESSION['mensagem'] = "FORMATO DE IDADE INVÁLIDO!";
 		header('Location: ../index.php');
 	} else {
-    $nome = mysqli_real_escape_string($connect,$_POST['nome']);
-    $sobrenome = mysqli_real_escape_string($connect,$_POST['sobrenome']);
-    $email = mysqli_real_escape_string($connect,$_POST['email']);
-    $idade = mysqli_real_escape_string($connect,$_POST['idade']);
+    $nome = clear($_POST['nome']);
+    $sobrenome = clear($_POST['sobrenome']);
+    $email = clear($_POST['email']);
+    $idade = clear($_POST['idade']);
 
     $sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome','$sobrenome','$email','$idade')";
     if(mysqli_query($connect,$sql)){
